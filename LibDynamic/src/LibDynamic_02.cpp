aa1d03ddace81c008eb9123495da0b7e207a8136
Cao Tri Le (uif63571) <cao.tri.le@continental-corporation.com>
2022-12-19 14:26:49 +0800

    demo linking

--- LibDynamic/src/LibDynamic_02.cpp
+++ LibDynamic/src/LibDynamic_02.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "LibDynamic_02.h"
+#include "example.h"
 
 void funcDynamic_02() {
     printf("Enter %s\n", __func__);
+    funcExample();
 }
---
+++ LibDynamic/src/example.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "example.h"
+
+void funcExample() {
+	printf("Enter %s\n", __func__);
+}
